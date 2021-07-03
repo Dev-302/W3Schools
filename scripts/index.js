a2f96728bbbd7220e2711c71f30ce59beb9d963f
Dev-302 <gebruiker302@outlook.com>
2021-07-03 17:32:38 +0200

    Various changes

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,17 @@
-var carName = "Volvo"
-var carName = "Porsche"
-var carName = "Lamvborginhi"
-var carName = "Maserati"
-var carName = "Mercedes"
-var carName = "BMW"
-var carName = "Volkswagen"
+var carName = "Volvo";
+var carName = "Porsche";
+var carName = "Lamborginhi";
+var carName = "Maserati";
+var carName = "Mercedes";
+var carName = "BMW";
+var carName = "Volkswagen";
 
 
 //One of many JavaScript HTML methods is "getElementByID()".
 // The example below 'finds' an HTML element (with id="demo"), and changes the element content (innerHTML) to "Hello JavaScript"
 document.getElementById("demo").innerHTML = "Hello JavaScript";
+
+let text = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+document.getElementById("demo").innerHTML = text.length;//will return 26
+
+//You can add quotes within the same "" by using \"QUOTES\"
---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+//Changes the "demo paragraph in HTML file"
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Paragraph changed.";
+}
+
+let x, y, z;  // Statement 1
+x = 5;        // Statement 2
+y = 8;        // Statement 3
+z = x + y;    // Statement 4
+
+//Changes the "demo1 paragraph in HTML file"
+document.getElementById("demo1").innerHTML = "The value of z is " + z + ".";
