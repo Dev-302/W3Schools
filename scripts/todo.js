654387511d68e957033220280ce2e80788a5294c
Dev-302 <gebruiker302@outlook.com>
2021-07-03 23:13:20 +0200

    Added todo.js

---
+++ scripts/todo.js
@@ -0,0 +1,37 @@
+function updateCounters() {
+    // Total number of todos
+    const totalCount = document.getElementById("total-count");
+    const totalTodos = document.getElementsByClassName("todo").length;
+    totalCount.innerHTML = totalTodos;
+  
+    // Total number of completed todos
+    const completedCount = document.getElementById("completed-count");
+    const completedTodos = document.getElementsByClassName("completed").length;
+    completedCount.innerHTML = completedTodos;
+  
+    // Total number of uncompleted todos
+    const todoCount = document.getElementById("todo-count");
+    const uncompletedTodos = totalTodos - completedTodos;
+    todoCount.innerHTML = uncompletedTodos;
+  }
+  
+  updateCounters();
+
+function toggleDone(event) {
+    const checkbox = event.currentTarget;
+    // check the checked status of the checkbox
+    if (checkbox.checked) {
+      // the "completed" class is set on the parent element, the <li>
+      checkbox.parentElement.parentElement.className = "todo completed";
+    } else {
+      checkbox.parentElement.parentElement.className = "todo";
+    }
+  
+    updateCounters();
+  }
+  
+  const checkboxes = document.querySelectorAll(".todo input");
+  
+  for (let i = 0; i < checkboxes.length; i++) {
+    checkboxes[i].addEventListener("change", toggleDone);
+  }
