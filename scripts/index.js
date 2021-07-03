a4132d4949e6ed70aa92a74a31881dd1d4131e35
Dev-302 <gebruiker302@outlook.com>
2021-07-03 14:59:40 +0200

    Created index file for JS tags

---
+++ scripts/index.js
@@ -0,0 +1,12 @@
+var carName = "Volvo"
+var carName = "Porsche"
+var carName = "Lamvborginhi"
+var carName = "Maserati"
+var carName = "Mercedes"
+var carName = "BMW"
+var carName = "Volkswagen"
+
+
+//One of many JavaScript HTML methods is "getElementByID()".
+// The example below 'finds' an HTML element (with id="demo"), and changes the element content (innerHTML) to "Hello JavaScript"
+document.getElementById("demo").innerHTML = "Hello JavaScript";
